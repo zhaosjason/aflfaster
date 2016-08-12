f02e7d326199b2a25bb57ed0ef80959629413138
Marcel Boehme <marcel.boehme@acm.org>
2016-08-13 02:18:18 +0800

    Typo

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -7776,7 +7776,7 @@ int main(int argc, char** argv) {
   gettimeofday(&tv, &tz);
   srandom(tv.tv_sec ^ tv.tv_usec ^ getpid());
 
-  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Q:p")) > 0)
+  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Q:p:")) > 0)
 
     switch (opt) {
 
@@ -7958,6 +7958,7 @@ int main(int argc, char** argv) {
         } else if (!stricmp(optarg, "explore")) {
           schedule = EXPLORE;
         }
+        break;
 
       default:
 
